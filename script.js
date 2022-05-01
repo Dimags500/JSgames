9e27363317fee046f9da10139cb4d244d7e2a67a
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-05-01 22:06:57 +0300

    basic proto , no rnd , winner , some state bug

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const startClock = document.getElementById("start-btn");
 startClock.addEventListener("click", startGame);
 const board = document.getElementById("game-board");
+let movesDisplay = document.getElementById("moves");
+let timeDisplay = document.getElementById("time");
+
+let moves = 0;
+let stateColor = "";
+let clickDisabled = false;
 
 function startGame(e) {
-  console.log(obj);
   if (!obj.inGame) {
+    timer();
     obj.inGame = !obj.inGame;
     cardBuilder(obj, board);
+    e.target.innerHTML = "Reset";
+    return;
   }
+  location.reload();
 }
 
 const obj = { inGame: false };
@@ -22,10 +31,44 @@ for (let i = 0; i < 6; i++) {
 function cardBuilder(obj, board) {
   for (let i = 0; i < 12; i++) {
     let card = document.createElement("div");
-    card.classList.add(obj[i], "card");
+    card.classList.add(obj[i], "card", "cover");
     card.addEventListener("click", cardClick);
     board.appendChild(card);
   }
 }
 
-function cardClick() {}
+function cardClick(e) {
+  console.log("state " + stateColor);
+
+  if (!clickDisabled) {
+    moves++;
+    movesDisplay.innerHTML = moves;
+    e.target.classList.remove("cover");
+    stateColor = cardCheck(e.target.classList[0], stateColor);
+
+    if (stateColor != "") {
+      clickDisabled = true;
+
+      setTimeout(() => {
+        e.target.classList.add("cover");
+        clickDisabled = false;
+      }, 2000);
+    }
+  }
+}
+
+function cardCheck(color, stateColor) {
+  if (color == stateColor) {
+    return "";
+  }
+  return color;
+}
+
+function timer() {
+  let start = Date.now();
+
+  setInterval(() => {
+    let delta = Date.now() - start;
+    timeDisplay.innerHTML = Math.floor(delta / 1000);
+  }, 1000);
+}
